252899c2df579c45be21ac546781702cfa2584d5
XLG123 <xiaolin.guan123@gmail.com>
2022-03-06 20:06:13 -0500

    modified the primary color and added a new google font to the blog

--- blog/lib/main.dart
+++ blog/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:blog/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Body());
@@ -12,7 +13,9 @@ class Body extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Blog Home', //title of the tab
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+        textTheme: GoogleFonts.neutonTextTheme(Theme.of(context).textTheme)),
       home: HomePage(), //this is the home page
     );
   }
