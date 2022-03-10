a4b0258b269dfe7af5e13f00a6be5144c7fd8efd
Rabiul Mazumder <rabiulmaz613@gmail.com>
2022-03-09 22:06:51 -0500

    Added title and made a card

--- blog/lib/pages/home.dart
+++ blog/lib/pages/home.dart
@@ -6,39 +6,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        //this creates an appbar at the top my page
-        title: Text('Blog Home',
-          style: 
-            TextStyle(
-              color: Colors.blue.shade800,
-              fontWeight: FontWeight.bold,
-              fontSize: 40),
-        ),
-        // A gradient appbar background.
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: <Color>[
+        appBar: AppBar(
+          //this creates an appbar at the top my page
+          title: Text(
+            'Blog Home',
+            style: TextStyle(
+                color: Colors.blue.shade800,
+                fontWeight: FontWeight.bold,
+                fontSize: 40),
+          ),
+          // A gradient appbar background.
+          flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: <Color>[
                 Colors.blue.shade100,
                 Colors.yellow.shade100,
-              ]
-              )
-          )
+              ]))),
+          centerTitle: true,
+          elevation: 0, //shadow below the appBar
+          toolbarHeight: 60,
         ),
-        centerTitle: true,
-        elevation: 0, //shadow below the appBar
-        toolbarHeight: 60,
-      ),
-      body: Column(
-        
-      )
-    );
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 26),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: [
+                    Text(
+                      "Welcome to Hunter's Voice",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 65,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Share your complaints anonymously,\n let everyone hear your thoughts.",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                        fontSize: 52,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Container(
+                      width: 285,
+                      decoration: BoxDecoration(
+                          border: Border(
+                              bottom:
+                                  BorderSide(color: Colors.black, width: 5))),
+                    ),
+                    SizedBox(height: 60),
+                    Container(
+                      width: 0.8 * MediaQuery.of(context).size.width,
+                      height: 400,
+                      child: Card(
+                        clipBehavior: Clip.antiAlias,
+                        child: Column(
+                            children: [Text("NO homeworks and NO projects")]),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
